fa3b686d6fc51ceb6bade57eab613387767435ba
Benny Yamagata <benny.yamagata@gmail.com>
2022-07-27 19:56:36 -0600

    Add main loop to program.cs to run multiple times

--- Program.cs
+++ Program.cs
@@ -2,77 +2,100 @@
 using DominionSimulator2;
 using DominionSimulator2.Data;
 
-// Prep the Supply
-CardDB.Cards = CardDB.Load();
-var supply = new SupplyPrep().CreateSupply();
+// Play the game X times
+const int MAX_GAMES = 100;
 
-// Prep the Players
-var players = new PlayerPrep().CreatePlayers(4);
-
-// Before the game begins, pick a random order of players:
-var random = new Random();
-players = players.OrderBy(x => random.Next()).ToList();
-
-// Play the game
-var gameOver = false;
-int turn = 1;
-while(!gameOver)
+for (int i = 0; i < MAX_GAMES; i++)
 {
-    Console.WriteLine($"Turn {turn}");
-    // Each player takes a turn
-    foreach (var player in players)
+    // Prep the Supply
+    CardDB.Cards = CardDB.Load();
+    var supply = new SupplyPrep().CreateSupply();
+
+    // Prep the Players
+    var players = new PlayerPrep().CreatePlayers(4);
+
+    // Before the game begins, pick a random order of players:
+    var random = new Random();
+    players = players.OrderBy(x => random.Next()).ToList();
+
+    // Play the game
+    var gameOver = false;
+    int turn = 1;
+    while (!gameOver)
     {
-        // Set Player Base Values
-        player.Actions = 1;
-        player.Buys = 1;
-        player.Coins = 0;
-
-        // Play Action Cards
-        player.PlayActions(supply);
-
-        // Buy Cards
-        player.BuyCards(supply);
-
-        // Player Ends Turn
-        player.EndTurn();
-
-        // Check if game is over
-        if(supply.IsGameOver())
+        // Each player takes a turn
+        foreach (var player in players)
         {
-            gameOver = true;
-            break;
+            // Set Player Base Values
+            player.Actions = 1;
+            player.Buys = 1;
+            player.Coins = 0;
+
+            // Play Action Cards
+            player.PlayActions(supply);
+
+            // Buy Cards
+            player.BuyCards(supply);
+
+            // Player Ends Turn
+            player.EndTurn();
+
+            // Check if game is over
+            if (supply.IsGameOver())
+            {
+                gameOver = true;
+                break;
+            }
         }
+        turn++;
     }
-    turn++;
+
+    // Update the CardDB and save to file
+    
 }
 
-// Check for winner
-var allCards = new List<Card>();
-foreach(var player in players)
-{
-    player.Cards.ResetDeck();
-    var points = player.Cards.GetVictoryPoints();
-    Console.WriteLine($"{player.Name} has {points} points");
-    player.Cards.GroupCards();
-    allCards.AddRange(player.Cards.Deck);
-}
 
-var cardWeights = new Dictionary<string, int>();
-foreach(var card in allCards)
-{
-    if(!cardWeights.Keys.Contains(card.Name))
-        cardWeights.Add(card.Name, 0);
-    cardWeights[card.Name] += card.Effects.Sum(e => (e is VictoryEffect) ? (e as VictoryEffect).Points : 0);
-}
 
-Console.WriteLine("\n\nCard Weights\n");
-foreach(var card in cardWeights.OrderByDescending(x => x.Value))
-{
-    CardDB.UpdateWeight(cardWeights[card.Key], card.Key);
-}
 
-Console.WriteLine("Card Name\t\tWeight\t\tTotal Quantity");
-foreach(var card in CardDB.Cards.OrderBy(c => c.Weight))
-{
-    Console.WriteLine($"{card.Name}\t\t\t{card.Weight}\t\t\t{allCards.Sum(x => x.Name == card.Name ? 1 : 0)}");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// // Check for winner
+// var allCards = new List<Card>();
+// foreach(var player in players)
+// {
+//     player.Cards.ResetDeck();
+//     var points = player.Cards.GetVictoryPoints();
+//     Console.WriteLine($"{player.Name} has {points} points");
+//     player.Cards.GroupCards();
+//     allCards.AddRange(player.Cards.Deck);
+// }
+
+// var cardWeights = new Dictionary<string, int>();
+// foreach(var card in allCards)
+// {
+//     if(!cardWeights.Keys.Contains(card.Name))
+//         cardWeights.Add(card.Name, 0);
+//     cardWeights[card.Name] += card.Effects.Sum(e => (e is VictoryEffect) ? (e as VictoryEffect).Points : 0);
+// }
+
+// Console.WriteLine("\n\nCard Weights\n");
+// foreach(var card in cardWeights.OrderByDescending(x => x.Value))
+// {
+//     CardDB.UpdateWeight(cardWeights[card.Key], card.Key);
+// }
+
+// Console.WriteLine("Card Name\t\tWeight\t\tTotal Quantity");
+// foreach(var card in CardDB.Cards.OrderBy(c => c.Weight))
+// {
+//     Console.WriteLine($"{card.Name}\t\t\t{card.Weight}\t\t\t{allCards.Sum(x => x.Name == card.Name ? 1 : 0)}");
+// }
